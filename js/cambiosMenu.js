9567070a419daa1bf66e90531fc6ac8742a71cab
daw2-LopezCarod22 <91276534+daw2-LopezCarod22@users.noreply.github.com>
2022-06-02 00:18:34 +0200

    Add files via upload

--- js/cambiosMenu.js
+++ js/cambiosMenu.js
@@ -1,28 +1,28 @@
 import {logout} from './logout.js';
 function cambiosMenus(){
     if(localStorage.getItem("token") != null){
-        var botonCarrito = `<form class="d-flex">
-            <button class="btn btn-outline-dark" type="submit">
-                <i class="bi-cart-fill me-1"></i>
-                <a id="carrito" href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/carrito.html">Carrito</a>
-                <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-            </button>
-        </form>`
+        var botonCarrito = `<a id="carrito" href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/carrito.html"><form class="d-flex">
+                                <button class="btn btn-outline-dark" type="button">
+                                    <i class="bi-cart-fill me-1"></i>
+                                    Carrito
+                                    <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                                </button>
+                            </a>`
         document.getElementById('carrito').innerHTML = botonCarrito;
         var botonCerrarSesion = `<button type="button" id="logout" class="sesion btn btn-danger">Cerrar Sesion</button>`
         document.getElementById('listaMenu').innerHTML += botonCerrarSesion;
-        var botonMiPerfil = `<button type="button" class="sesion btn btn-success"><a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/miPerfil.html">Mi perfil</a></button>`
+        var botonMiPerfil = `<a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/miPerfil.html"><button type="button" class="sesion btn btn-success">Mi perfil</button></a>`
         document.getElementById('listaMenu').innerHTML += botonMiPerfil;
         if(localStorage.getItem("username") == 'admin'){
-            var botonPanelAdministrador = `<button type="button" id="panelAdministrador" class="sesion btn btn-secondary"><a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/panelAdministrador.html">Panel de administrador</a></button>`
+            var botonPanelAdministrador = `<a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/panelAdministrador.html"><button type="button" id="panelAdministrador" class="sesion btn btn-secondary">Panel de administrador</button></a>`
             document.getElementById('listaMenu').innerHTML += botonPanelAdministrador;
         }
         document.getElementById('logout').onclick = function(){
             logout();
         }
     } else{
-        var botonIniciarRegistrarse = `<button type="button" class="sesion btn btn-primary"><a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/login.html">Iniciar Sesion</a></button>
-        <button type="button" class="sesion btn btn-success"><a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/register.html">Registrarse</a></button>`
+        var botonIniciarRegistrarse = `<a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/login.html"><button type="button" class="sesion btn btn-primary">Iniciar Sesion</button></a>
+        <a href="https://daw2-lopezcarod22.github.io/kameshop.github.io/html/register.html"><button type="button" class="sesion btn btn-success">Registrarse</button></a>`
         document.getElementById('listaMenu').innerHTML += botonIniciarRegistrarse;
     }
 }
